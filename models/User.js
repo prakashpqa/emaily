9c16e90b8387d7aa027526c6c638d4aadc5736c0
Prakash <prakashprabhakarqa@gmail.com>
2017-08-13 16:05:26 +0530

    ...

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 // const Schema = mongoose.Schema; same as below
-const { Schema } = mongoose;
+const {Schema} = mongoose;
 
 const userSchema = new Schema({
-  googleId: String
+    googleId: String
 });
 
 mongoose.model('users', userSchema);
--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,32 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    }
-  )
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            // console.log('accessToken', accessToken);
+            // console.log('refreshToken', refreshToken);
+            // console.log('profile', profile);
+            User.findOne({googleId: profile.id}).then((existingUser) => {
+                "use strict";
+                if (existingUser) {
+                    // we already have a record with the given profile ID
+                    done(null, existingUser)
+                } else {
+                    // make a new record
+                    new User({googleId: profile.id}).save();
+                }
+            })
+
+
+        }
+    )
 );
